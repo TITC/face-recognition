53d1a8b50725577632b0936fa74cba251c4bf9fb
Yuhang Tao <35098797+TITC@users.noreply.github.com>
2021-03-10 20:11:30 +0800

    Update __init__.py

--- facerecognition/eigenfaces/__init__.py
+++ facerecognition/eigenfaces/__init__.py
@@ -60,7 +60,7 @@ class EigenFaces(object):
                 min_class = self.labels_list[i]
         print(min_class, min_distance)
         if min_distance>3500:#threshold
-            min_class = "Alien"
+            min_class = "unknown identity"
         return min_class
 
     def __repr__(self):
